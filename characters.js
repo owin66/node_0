945bf03c1cc9f87aeae8b44c491b73c12ca6a2d9
owin66 <78900270+owin66@users.noreply.github.com>
2022-06-18 12:09:51 +0300

    app_LOTR

---
+++ characters.js
@@ -0,0 +1,14 @@
+let characters = [
+    {name: 'Frodo', hasRing: false},
+    {name: 'Bilbo', hasRing: false},
+];
+
+function stealRing(characters, owner) {
+    characters.map(c => {
+        if (c.name === owner) {
+            c.hasRing = true
+        } else {
+            c.hasRing = false
+        }
+    })
+}
