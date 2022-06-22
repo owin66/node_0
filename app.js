17b3497b1ee20017f7d2e5479da892f0db60db56
owin66 <78900270+owin66@users.noreply.github.com>
2022-06-22 07:29:00 +0300

    EventEmitter_module

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log(require)
+const EventEmitter = require('events')
+
+const myEmitter = new EventEmitter();
+
+const logDbConnection = () => {
+    console.log('DB connection')
+}
+
+myEmitter.addListener('connected', logDbConnection);
